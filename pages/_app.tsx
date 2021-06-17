141798a4896f65161d5cf64393f8e0bb38dafc2c
Mito6211 <willmoore674@gmail.com>
2021-06-17 18:37:49 -0400

    Initial setup with chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider, Container } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Container maxW="1200px">
+        <Component {...pageProps} />
+      </Container>
+    </ChakraProvider>
+  );
 }
-export default MyApp
+
+export default MyApp;
