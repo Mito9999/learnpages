9308f03aa915e4d51c431b233a131715f0a5144f
Mito6211 <willmoore674@gmail.com>
2021-06-19 10:52:36 -0400

    Connect API to frontend, fix static generation

--- pages/learn/[uid].tsx
+++ pages/learn/[uid].tsx
@@ -1,32 +1,57 @@
-import { Text } from "@chakra-ui/react";
+import { Text, Spinner, Heading, Image, Badge, HStack } from "@chakra-ui/react";
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { serverUrl } from "../../utils";
+import { useRouter } from "next/router";
+
+const currencyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
 
 const Page: NextPage = ({ pageData }: any) => {
-  return <Text>/{pageData.uid}</Text>;
+  const { isFallback } = useRouter();
+
+  return (
+    <>
+      {isFallback ? (
+        <Spinner />
+      ) : (
+        <>
+          <Heading>{pageData.title}</Heading>
+          <Image src={pageData.image} alt={pageData.title + " cover image"} />
+          <HStack spacing="8px" my="10px">
+            {pageData.tags.map((tag: string, i: number) => (
+              <Badge key={tag + " tag " + i}>{tag}</Badge>
+            ))}
+          </HStack>
+          <Text>Estimated Cost: {currencyFormatter.format(pageData.cost)}</Text>
+          <Text>Estimated Time: {pageData.hours} hours</Text>
+          <Text>
+            Created: {new Date(pageData.dateCreated).toLocaleString()}
+          </Text>
+          <Text>Edited: {new Date(pageData.dateEdited).toLocaleString()}</Text>
+        </>
+      )}
+    </>
+  );
 };
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const res = await fetch(serverUrl + "/api/uid-list");
-  const data: { uids: string[] } = await res.json();
-  const paramList = data.uids.map((uid) => ({ params: { uid } }));
-
-  return {
-    paths: paramList,
-    fallback: false,
-  };
+export const getStaticPaths: GetStaticPaths = () => {
+  return { paths: [], fallback: true };
 };
 
 export const getStaticProps: GetStaticProps = async (context: any) => {
   const { uid } = context.params;
-  const res = await fetch(serverUrl + "/api/data/" + uid);
-  const pageData = await res.json();
 
-  return {
-    props: {
-      pageData: { uid },
-    },
-  };
+  try {
+    const res = await fetch(serverUrl + "/api/data/" + uid);
+    if (!res.ok) throw new Error("Not found");
+
+    const pageData = await res.json();
+    return { props: { pageData } };
+  } catch {
+    return { notFound: true };
+  }
 };
 
 export default Page;
