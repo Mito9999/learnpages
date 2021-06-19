557c27d4c64907b20135b3e80a0ad0fedc5867a8
Mito6211 <willmoore674@gmail.com>
2021-06-19 12:17:23 -0400

    Add animation and links

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import {
   Stack,
   Badge,
   Text,
+  AspectRatio,
 } from "@chakra-ui/react";
 import { serverUrl } from "../utils";
 
@@ -22,44 +23,48 @@ const Home: NextPage = ({ pages }: any) => {
       </Head>
 
       <Heading>Learnpages</Heading>
-      <SimpleGrid columns={[1, 2, 3]} spacing={4}>
+      <SimpleGrid columns={[1, 2, 3]} spacing={6}>
         {pages.map((page: any) => (
-          <Box
-            key={page.uid}
-            borderRadius="20px"
-            border="1px solid"
-            borderColor="gray.300"
-          >
-            <Image
-              src={page.image}
-              alt={page.title + " cover image"}
-              objectFit="cover"
-              w="100%"
-              h={["200px", "calc(56.25% / 2)", "calc(56.25% / 3"]}
-              borderTopRadius="20px"
-            />
-            <Box p="15px">
-              <Heading as="h3" size="md">
-                {page.title}
-              </Heading>
-              <Stack direction="row" spacing="8px" my="10px">
-                {page.tags.map((tag: string, i: number) => (
-                  <Badge key={tag + " tag " + i} w="fit-content">
-                    {tag}
-                  </Badge>
-                ))}
-              </Stack>
-              <Text>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-                iste fugiat totam voluptatum placeat quis autem ipsam cupiditate
-                esse inventore quibusdam, nam quos unde provident dolore modi,
-                nesciunt ea delectus dolorum at velit tempora. Excepturi
-                sapiente eaque aliquam exercitationem, aspernatur modi natus
-                nostrum magnam perspiciatis necessitatibus vitae similique
-                laborum eum.
-              </Text>
+          <Link key={page.uid} href={`/learn/${page.uid}`} passHref={true}>
+            <Box
+              borderRadius="20px"
+              border="1px solid"
+              borderColor="gray.300"
+              cursor="pointer"
+              _hover={{ transform: "scale(1.03)" }}
+              transition="transform 150ms ease"
+            >
+              <AspectRatio ratio={16 / 9}>
+                <Image
+                  src={page.image}
+                  alt={page.title + " cover image"}
+                  objectFit="cover"
+                  borderTopRadius="20px"
+                />
+              </AspectRatio>
+              <Box p="15px">
+                <Heading as="h3" size="md">
+                  {page.title}
+                </Heading>
+                <Stack direction="row" spacing="8px" my="10px">
+                  {page.tags.map((tag: string, i: number) => (
+                    <Badge key={tag + " tag " + i} w="fit-content">
+                      {tag}
+                    </Badge>
+                  ))}
+                </Stack>
+                <Text>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Beatae iste fugiat totam voluptatum placeat quis autem ipsam
+                  cupiditate esse inventore quibusdam, nam quos unde provident
+                  dolore modi, nesciunt ea delectus dolorum at velit tempora.
+                  Excepturi sapiente eaque aliquam exercitationem, aspernatur
+                  modi natus nostrum magnam perspiciatis necessitatibus vitae
+                  similique laborum eum.
+                </Text>
+              </Box>
             </Box>
-          </Box>
+          </Link>
         ))}
       </SimpleGrid>
     </>
